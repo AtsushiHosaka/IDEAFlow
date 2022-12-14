d3725e04f8504e876dc0744ad811229734413560
AtsushiHosaka <hosaka2917@gmail.com>
2022-12-14 11:26:02 +0900

    細かい修正を加えた

--- IDEAFlow/Models/FlowManager.swift
+++ IDEAFlow/Models/FlowManager.swift
@@ -19,8 +19,8 @@ class FlowManager {
     let timeRimit = 60
     var currentTime: Int = 0
     
-    let components_functions = ["カメラ", "画像認識", "メモ", "AR", "VR"]
-    let components_consepts = ["単語学習", "勉強", "SNS", "運動", "ゲーム"]
+    let components_functions = ["カメラ", "画像認識", "メモ", "AR", "VR", "時計", "アラーム", "カレンダー", "ストップウォッチ", "地図", "天気", "位置情報", "加速度センサー", "明るさセンサー", "ライト", "通話", "電卓", "AI", "キーボード", "通知"]
+    let components_consepts = ["単語学習", "勉強", "SNS", "運動", "ゲーム", "リマインダー", "時間割", "宿題", "チャット", "ノート", "散歩", "スポーツ", "服装", "タイピング", "睡眠", "砂時計", "音楽", "プレイリスト", "食事", "買い物", "プログラミング", "部活", "おかね", "読書", "公園", "電車", "黒板", "空", "星座", "文化", "デザイン", "映え"]
     var currentComponents = (component1: "", component2: "")
     
     func getComponentsPair() {
